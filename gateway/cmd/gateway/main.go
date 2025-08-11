7ac7d8e6b9a0e1955fa43844344614ca08164095
Brian VanLoo <brian.vanloo@gmail.com>
2025-08-10 21:49:33 -0700

    Implement minimum to get create bucket test passing + CI (#1)

--- gateway/cmd/gateway/main.go
+++ gateway/cmd/gateway/main.go
@@ -7,6 +7,14 @@ import (
 
 func newMux() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPut && r.URL.Path != "/" {
+			w.Header().Set("Location", r.URL.Path)
+			w.WriteHeader(http.StatusCreated)
+			return
+		}
+		http.NotFound(w, r)
+	})
 	return mux
 }
 
