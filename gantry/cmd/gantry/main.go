79a61e2d71380dad5298a26945faa879f2a684bd
Brian VanLoo <brian.vanloo@gmail.com>
2025-09-01 09:23:48 -0700

    Add a skeleton create bucket gRPC service to gantry

--- gantry/cmd/gantry/main.go
+++ gantry/cmd/gantry/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"log/slog"
+	"net"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
-)
 
-var version = "0.0.1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/ratdaddy/blockcloset/gantry/internal/grpcsvc"
+)
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
@@ -17,9 +22,46 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 
-	logger.Info("gantryd starting", "version", version)
-	<-ctx.Done()
+	addr := ":8081"
 
-	logger.Info("gantryd shutting down")
-	time.Sleep(200 * time.Millisecond)
+	logger.Info("gantry starting", "addr", addr)
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		logger.Error("listen failed", "err", err)
+		os.Exit(1)
+	}
+
+	s := grpc.NewServer()
+
+	grpcsvc.Register(s)
+
+	enable_reflection, _ := strconv.ParseBool(os.Getenv("ENABLE_REFLECTION"))
+	if enable_reflection {
+		reflection.Register(s)
+	}
+
+	errCh := make(chan error, 1)
+	go func() { errCh <- s.Serve(lis) }()
+
+	select {
+	case <-ctx.Done():
+		logger.Info("shutdown signal received")
+		// graceful stop with a short cap, then force
+		done := make(chan struct{})
+		go func() {
+			s.GracefulStop()
+			close(done)
+		}()
+		select {
+		case <-done:
+			logger.Info("grpc server stopped gracefully")
+		case <-time.After(3 * time.Second):
+			logger.Warn("graceful stop timed out; forcing")
+			s.Stop()
+		}
+	case err := <-errCh:
+		// Serve exited on its own (listener closed or fatal error)
+		logger.Error("grpc serve exited", "err", err)
+	}
 }
