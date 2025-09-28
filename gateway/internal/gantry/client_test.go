f8ffe5c45abb7d5f6e60042d33d3d6c5fa74d30e
Brian VanLoo <brian.vanloo@gmail.com>
2025-09-27 21:18:37 -0700

    Add contract test for client create_bucket

--- gateway/internal/gantry/client_test.go
+++ gateway/internal/gantry/client_test.go
@@ -2,60 +2,17 @@ package gantry
 
 import (
 	"context"
-	"net"
 	"testing"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/test/bufconn"
-
 	"github.com/ratdaddy/blockcloset/gateway/internal/httpapi"
-	bucketv1 "github.com/ratdaddy/blockcloset/proto/gen/gantry/bucket/v1"
-	servicev1 "github.com/ratdaddy/blockcloset/proto/gen/gantry/service/v1"
 )
-
-const bufSize = 1024 * 1024
-
-type captureService struct {
-	servicev1.UnimplementedGantryServiceServer
-	lastIDs []string
-}
-
-func (s *captureService) CreateBucket(ctx context.Context, req *servicev1.CreateBucketRequest) (*servicev1.CreateBucketResponse, error) {
-	if md, ok := metadata.FromIncomingContext(ctx); ok {
-		s.lastIDs = append([]string{}, md.Get("x-request-id")...)
-	} else {
-		s.lastIDs = nil
-	}
-	return &servicev1.CreateBucketResponse{Bucket: &bucketv1.Bucket{Name: req.GetName()}}, nil
-}
 
 func TestClientRequestIDPropagation(t *testing.T) {
 	t.Parallel()
 
-	lis := bufconn.Listen(bufSize)
-	srv := grpc.NewServer()
-	svc := &captureService{}
-	servicev1.RegisterGantryServiceServer(srv, svc)
+	client, svc := newTestClient(t)
 
-	go func() {
-		if err := srv.Serve(lis); err != nil && err != grpc.ErrServerStopped {
-			panic(err)
-		}
-	}()
-	t.Cleanup(func() { srv.Stop() })
-
-	client, err := New(context.Background(), "passthrough:///bufnet", grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
-		return lis.DialContext(ctx)
-	}))
-	if err != nil {
-		t.Fatalf("New: %v", err)
-	}
-	t.Cleanup(func() {
-		if err := client.Close(); err != nil {
-			t.Fatalf("Close: %v", err)
-		}
-	})
+	const bucketName = "case-bucket"
 
 	cases := []struct {
 		name string
@@ -76,13 +33,18 @@ func TestClientRequestIDPropagation(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			svc.lastIDs = nil
+			svc.Reset()
 
-			if _, err := client.CreateBucket(tc.ctx(t), "case-bucket"); err != nil {
+			if _, err := client.CreateBucket(tc.ctx(t), bucketName); err != nil {
 				t.Fatalf("CreateBucket: %v", err)
 			}
 
-			got := svc.lastIDs
+			call, ok := svc.LastCreateBucketCall()
+			if !ok {
+				t.Fatalf("expected CreateBucket call")
+			}
+
+			got := call.Metadata.Get("x-request-id")
 
 			if len(got) != len(tc.want) {
 				t.Fatalf("request id count: got %d want %d (values %v)", len(got), len(tc.want), got)
